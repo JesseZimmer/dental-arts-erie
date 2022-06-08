5e6a84bf147f50fe110785827e21a1003f317516
JesseZimmer <jzimmer814@gmail.com>
2022-06-07 22:57:26 -0400

    Moved header and footer to components folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
+import Header from '../components/header/header'
+import Footer from '../components/footer/footer'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <div>
+    <Header />
+  <Component {...pageProps} />
+  <Footer />
+  </div>
+  )
 }
 
 export default MyApp
